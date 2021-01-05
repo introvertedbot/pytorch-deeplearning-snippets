90000402829d2f0082890e10b8a0d3f2cb7b87fb
introvertedbot <introvertedbot@protonmail.com>
2021-01-05 20:22:23 +0530

    Finished building the network

--- Diabetes.py
+++ Diabetes.py
@@ -59,4 +59,28 @@ for x,y in train_loader:
     print(f"Labels:    {y.shape}")
     break
 
+class Model(nn.Module):
+    def __init__(self, input_features, output_features):
+        super(Model, self).__init__()
+        self.fc1 = nn.Linear(input_features, 5)
+        self.fc2 = nn.Linear(5, 4)
+        self.fc3 = nn.Linear(4, 3)
+        self.fc4 = nn.Linear(3, output_features)
+        self.sigmoid = nn.Sigmoid()
+        self.tanh = nn.Tanh()
+    
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.tanh(out)
+        out = self.fc2(out)
+        out = self.tanh(out)
+        out = self.fc3(out)
+        out = self.tanh(out)
+        out = self.fc4(out)
+        out = self.sigmoid(out)
+        return out
 
+net = Model(7, 1)
+criterion = torch.nn.BCELoss(size_average=True)
+optimizer = torch.optim.SGD(net.parameters(), lr = 0.1, momentum = 0.9)
+        
