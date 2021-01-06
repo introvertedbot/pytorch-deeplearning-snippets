19aa16ff105d68dbe112dcd9ff9c9f0c5d926ecc
introvertedbot <introvertedbot@protonmail.com>
2021-01-06 20:39:45 +0530

    Finished setting up batch size

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -9,6 +9,7 @@ import torch
 import torch.nn as nn
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
 
 mean_gray = 0.1307
 stddev_gray = 0.3081
@@ -25,4 +26,15 @@ test_dataset = datasets.MNIST(root = 'D:\\',
                                train = False,
                                transform = transforms,
                                download = True)
+
+random_img = train_dataset[20][0].numpy() * stddev_gray + mean_gray
+plt.imshow(random_img.reshape(28,28), cmap = 'gray')
+
+batch_size = 100
+train_load = torch.utils.data.DataLoader(dataset=train_dataset,
+                                         batch_size = batch_size,
+                                         shuffle = True)
+test_load = torch.utils.data.DataLoader(dataset = test_dataset,
+                                         batch_size = batch_size,
+                                         shuffle = False)
 
