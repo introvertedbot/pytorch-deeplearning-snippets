e6d5e592ad9bc4976c2153d1f42b3d29d60b66a8
introvertedbot <introvertedbot@protonmail.com>
2021-01-07 10:36:07 +0530

    Finished building the model

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -38,3 +38,42 @@ test_load = torch.utils.data.DataLoader(dataset = test_dataset,
                                          batch_size = batch_size,
                                          shuffle = False)
 
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        # Same padding --> input_size = output_size
+        # Same padding = (filter_size - 1) / 2 --> (3 - 1)/2 = 1
+        self.cnn1 = nn.Conv2d(in_channels = 1, out_channels = 8, kernel_size = 3, stride = 1, padding = 1)
+        # the output size of each of the 8 feature maps:
+        # [(input_size - filtersize + 2(padding))/stride + 1] = ( 28 - 3 + 2 )1 + 1 = 28
+        self.batchnorm1 = nn.BatchNorm2d(8)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size = 2)
+        # the output size = 28/2 = 14
+        # Same padding = (5 - 1)/2 = 2
+        self.cnn2 = nn.Conv2d(in_channels = 8, out_channels = 32, kernel_size = 5, stride = 1, padding = 2)
+        # output size of each of the 32 feature maps [(14-5 + 2(2)/1 + 1)] = 14
+        self.batchnorm2 = nn.BatchNorm2d(32)
+        # Flatten the 32 feature maps: 7*7*32 = 1568
+        self.fc1 = nn.Linear(1568, 600)
+        self.dropout = nn.Dropout(p = 0.5)
+        self.fc2 = nn.Linear(600, 10)
+        
+    def forward(self, x):
+        out = self.cnn1(x)
+        out = self.batchnorm1(out)
+        out = self.relu(out)
+        out = self.maxpool(out)
+        out = self.cnn2(out)
+        out = self.batchnorm2(32)
+        out = self.relu(out)
+        out = self.maxpool(out)
+        # Flatten the 32 feature maps from max pool to feed it to FC1(100, 1568)
+        out = self.view(-1, 1568)
+        # then we forward through our fully connected layer
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.dropout(out)
+        out = self.fc2(out)
+        return out
+        
