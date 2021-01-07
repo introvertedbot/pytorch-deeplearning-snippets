f63b1766b67139e1d153a4a47aaad79cb182b784
introvertedbot <introvertedbot@protonmail.com>
2021-01-07 11:16:16 +0530

    Finished training the model

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -38,42 +38,107 @@ test_load = torch.utils.data.DataLoader(dataset = test_dataset,
                                          batch_size = batch_size,
                                          shuffle = False)
 
+#Create the model class
 class CNN(nn.Module):
     def __init__(self):
-        super(CNN, self).__init__()
-        # Same padding --> input_size = output_size
-        # Same padding = (filter_size - 1) / 2 --> (3 - 1)/2 = 1
-        self.cnn1 = nn.Conv2d(in_channels = 1, out_channels = 8, kernel_size = 3, stride = 1, padding = 1)
-        # the output size of each of the 8 feature maps:
-        # [(input_size - filtersize + 2(padding))/stride + 1] = ( 28 - 3 + 2 )1 + 1 = 28
+        super(CNN,self).__init__()
+        #Same Padding = [(filter size - 1) / 2] (Same Padding--> input size = output size)
+        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3,stride=1, padding=1)
+        #The output size of each of the 8 feature maps is 
+        #[(input_size - filter_size + 2(padding) / stride) +1] --> [(28-3+2(1)/1)+1] = 28 (padding type is same)
+        #Batch normalization
         self.batchnorm1 = nn.BatchNorm2d(8)
+        #RELU
         self.relu = nn.ReLU()
-        self.maxpool = nn.MaxPool2d(kernel_size = 2)
-        # the output size = 28/2 = 14
-        # Same padding = (5 - 1)/2 = 2
-        self.cnn2 = nn.Conv2d(in_channels = 8, out_channels = 32, kernel_size = 5, stride = 1, padding = 2)
-        # output size of each of the 32 feature maps [(14-5 + 2(2)/1 + 1)] = 14
+        self.maxpool = nn.MaxPool2d(kernel_size=2)
+        #After max pooling, the output of each feature map is now 28/2 = 14
+        self.cnn2 = nn.Conv2d(in_channels=8, out_channels=32, kernel_size=5, stride=1, padding=2)
+        #Output size of each of the 32 feature maps remains 14
         self.batchnorm2 = nn.BatchNorm2d(32)
-        # Flatten the 32 feature maps: 7*7*32 = 1568
-        self.fc1 = nn.Linear(1568, 600)
-        self.dropout = nn.Dropout(p = 0.5)
-        self.fc2 = nn.Linear(600, 10)
-        
-    def forward(self, x):
+        #After max pooling, the output of each feature map is 14/2 = 7
+        #Flatten the feature maps. You have 32 feature maps, each of them is of size 7x7 --> 32*7*7 = 1568
+        self.fc1 = nn.Linear(in_features=1568, out_features=600)
+        self.droput = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(in_features=600, out_features=10)
+    def forward(self,x):
         out = self.cnn1(x)
         out = self.batchnorm1(out)
         out = self.relu(out)
         out = self.maxpool(out)
         out = self.cnn2(out)
-        out = self.batchnorm2(32)
+        out = self.batchnorm2(out)
         out = self.relu(out)
         out = self.maxpool(out)
-        # Flatten the 32 feature maps from max pool to feed it to FC1(100, 1568)
-        out = self.view(-1, 1568)
-        # then we forward through our fully connected layer
+        #Now we have to flatten the output. This is where we apply the feed forward neural network as learned before! 
+        #It will take the shape (batch_size, 1568) = (100, 1568)
+        out = out.view(-1,1568)
+        #Then we forward through our fully connected layer 
         out = self.fc1(out)
         out = self.relu(out)
-        out = self.dropout(out)
+        out = self.droput(out)
         out = self.fc2(out)
         return out
+
+
+model = CNN()
+CUDA = torch.cuda.is_available()
+if CUDA:
+    model = model.cuda()    
+loss_fn = nn.CrossEntropyLoss()        
+# Show weight decay
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
+    
+#Understand what's happening
+iteration = 0
+correct = 0
+
+for i,(inputs,labels) in enumerate (train_load):
+
+    if CUDA:
+        inputs = inputs.cuda()
+        labels = labels.cuda()
+        
+    print("For one iteration, this is what happens:")
+    print("Input Shape:",inputs.shape)
+    print("Labels Shape:",labels.shape)
+    output = model(inputs)
+    print("Outputs Shape",output.shape)
+    _, predicted = torch.max(output, 1)
+    print("Predicted Shape",predicted.shape)
+    print("Predicted Tensor:")
+    print(predicted)
+    correct += (predicted == labels).sum()
+    break
+
+# Training the CNN
+num_epohs = 10
+train_loss = []
+train_accuracy = []
+test_loss = []
+test_accuracy = []
+for epoch in range(num_epohs):
+    correct = 0
+    iterations = 0
+    iter_loss = 0.0
+    
+    model.train()
+    
+    for i,(inputs, labels) in enumerate(train_load):
+        if CUDA:
+            inputs = inputs.cuda()
+            labels = labels.cuda()
+        outputs = model(inputs)
+        loss = loss_fn(outputs, labels)
+        iter_loss += loss.item()
+        
+        optimizer.zero_grad() # w <-- w - lr*gradient
+        loss.backward()
+        optimizer.step()
+        
+        _, predicted = torch.max(outputs, 1)
+        correct += (predicted == labels).sum().item()
+        iterations += 1
+    
+    train_loss.append(iter_loss/iterations())
+    train_accuracy.append(correct/ len(train_dataset))
         
