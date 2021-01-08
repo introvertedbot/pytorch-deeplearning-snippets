cb1a7502e88c859f64bb054a3a54d9bcce0c3ee8
introvertedbot <introvertedbot@protonmail.com>
2021-01-08 17:10:41 +0530

    Finished visualizing CNN

--- cnn_visualization.py
+++ cnn_visualization.py
@@ -24,7 +24,7 @@ transform = transforms.Compose([transforms.Resize((224,224)),
                                 transforms.ToTensor(),
                                 transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])])
 
-image = Image.open(r'C:\Users\IntrovertedBot\Downloads\dog.jpg')
+image = Image.open('dog.jpg')
 plt.imshow(image)
 
 vgg = models.vgg16(pretrained=True)
@@ -40,3 +40,79 @@ if CUDA:
 image = transform(image)
 
 print(image.shape)
+
+#Add the batch size
+image = image.unsqueeze(0)
+
+#Wrap it up in a variable
+image = Variable(image)
+
+#Transfer it to the GPU
+if CUDA:
+    image = image.cuda()
+
+
+print(image.shape)
+
+output = vgg(image)
+
+print(output.shape)
+
+#Transfer the 2D Tensor to 1D
+output = output.squeeze(0)
+
+print(output.shape)
+
+labels = json.load(open('imagenet_class_index.json'))
+
+index = output.max(0)
+
+print(index)
+
+index = str(index[1][0].item())
+label = labels[index][1]
+
+print(label)
+
+
+
+module_list = list(vgg.features.modules())
+
+
+
+print(vgg.features)
+print(module_list[0])
+print(module_list[1])
+print(module_list[2])
+module_list
+
+outputs = []
+names = []
+for layer in module_list[1:]:
+    image = layer(image)
+    outputs.append(image)
+    names.append(str(layer))
+
+for feature_map in outputs:
+    print(feature_map.shape)
+
+processed = []
+for feature_map in outputs:
+    feature_map = feature_map.squeeze(0)
+    #Convert the 3D Tensor to 2D. Sum the same element of every channel
+    gray_scale = torch.sum(feature_map, 0)
+    gray_scale = gray_scale/feature_map.shape[0]
+    processed.append(gray_scale.data.cpu().numpy())
+
+processed = []
+for feature_map in outputs:
+    feature_map = feature_map.squeeze(0)
+    #Convert the 3D Tensor to 2D. Sum the same element of every channel
+    gray_scale = torch.sum(feature_map, 0)
+    gray_scale = gray_scale/feature_map.shape[0]
+    processed.append(gray_scale.data.cpu().numpy())
+
+
+
+
+
