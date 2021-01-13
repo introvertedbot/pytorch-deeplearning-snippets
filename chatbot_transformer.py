4e9b994a533bb4b349188b67ab6084c2affeedec
introvertedbot <introvertedbot@protonmail.com>
2021-01-13 12:12:15 +0530

    Finished label smoothing

--- chatbot_transformer.py
+++ chatbot_transformer.py
@@ -300,3 +300,28 @@ class AdamWarmup:
         # update the learning rate
         self.lr = lr
         self.optimizer.step()
+        
+class LossWithLS(nn.Module):
+
+    def __init__(self, size, smooth):
+        super(LossWithLS, self).__init__()
+        self.criterion = nn.KLDivLoss(size_average=False, reduce=False)
+        self.confidence = 1.0 - smooth
+        self.smooth = smooth
+        self.size = size
+        
+    def forward(self, prediction, target, mask):
+        """
+        prediction of shape: (batch_size, max_words, vocab_size)
+        target and mask of shape: (batch_size, max_words)
+        """
+        prediction = prediction.view(-1, prediction.size(-1))   # (batch_size * max_words, vocab_size)
+        target = target.contiguous().view(-1)   # (batch_size * max_words)
+        mask = mask.float()
+        mask = mask.view(-1)       # (batch_size * max_words)
+        labels = prediction.data.clone()
+        labels.fill_(self.smooth / (self.size - 1))
+        labels.scatter_(1, target.data.unsqueeze(1), self.confidence)
+        loss = self.criterion(prediction, labels)    # (batch_size * max_words, vocab_size)
+        loss = (loss.sum(1) * mask).sum() / mask.sum()
+        return loss
