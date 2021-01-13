84e2d07661c7262c66f865c8d906ffbec9cddb84
introvertedbot <introvertedbot@protonmail.com>
2021-01-13 12:14:56 +0530

    Finished defining the training function

--- chatbot_transformer.py
+++ chatbot_transformer.py
@@ -341,3 +341,42 @@ transformer = transformer.to(device)
 adam_optimizer = torch.optim.Adam(transformer.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9)
 transformer_optimizer = AdamWarmup(model_size = d_model, warmup_steps = 4000, optimizer = adam_optimizer)
 criterion = LossWithLS(len(word_map), 0.1)
+
+def train(train_loader, transformer, criterion, epoch):
+    
+    transformer.train()
+    sum_loss = 0
+    count = 0
+
+    for i, (question, reply) in enumerate(train_loader):
+        
+        samples = question.shape[0]
+
+        # Move to device
+        question = question.to(device)
+        reply = reply.to(device)
+
+        # Prepare Target Data
+        reply_input = reply[:, :-1]
+        reply_target = reply[:, 1:]
+
+        # Create mask and add dimensions
+        question_mask, reply_input_mask, reply_target_mask = create_masks(question, reply_input, reply_target)
+
+        # Get the transformer outputs
+        out = transformer(question, question_mask, reply_input, reply_input_mask)
+
+        # Compute the loss
+        loss = criterion(out, reply_target, reply_target_mask)
+        
+        # Backprop
+        transformer_optimizer.optimizer.zero_grad()
+        loss.backward()
+        transformer_optimizer.step()
+        
+        sum_loss += loss.item() * samples
+        count += samples
+        
+        if i % 100 == 0:
+            print("Epoch [{}][{}/{}]\tLoss: {:.3f}".format(epoch, i, len(train_loader), sum_loss/count))
+            
