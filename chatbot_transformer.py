9d30dfc3c4198c286730a6a3bb92f6fc5351bc07
introvertedbot <introvertedbot@protonmail.com>
2021-01-13 12:16:13 +0530

    Finished defining evaluation function

--- chatbot_transformer.py
+++ chatbot_transformer.py
@@ -380,3 +380,34 @@ def train(train_loader, transformer, criterion, epoch):
         if i % 100 == 0:
             print("Epoch [{}][{}/{}]\tLoss: {:.3f}".format(epoch, i, len(train_loader), sum_loss/count))
             
+def evaluate(transformer, question, question_mask, max_len, word_map):
+    """
+    Performs Greedy Decoding with a batch size of 1
+    """
+    rev_word_map = {v: k for k, v in word_map.items()}
+    transformer.eval()
+    start_token = word_map['<start>']
+    encoded = transformer.encode(question, question_mask)
+    words = torch.LongTensor([[start_token]]).to(device)
+    
+    for step in range(max_len - 1):
+        size = words.shape[1]
+        target_mask = torch.triu(torch.ones(size, size)).transpose(0, 1).type(dtype=torch.uint8)
+        target_mask = target_mask.to(device).unsqueeze(0).unsqueeze(0)
+        decoded = transformer.decode(words, target_mask, encoded, question_mask)
+        predictions = transformer.logit(decoded[:, -1])
+        _, next_word = torch.max(predictions, dim = 1)
+        next_word = next_word.item()
+        if next_word == word_map['<end>']:
+            break
+        words = torch.cat([words, torch.LongTensor([[next_word]]).to(device)], dim = 1)   # (1,step+2)
+        
+    # Construct Sentence
+    if words.dim() == 2:
+        words = words.squeeze(0)
+        words = words.tolist()
+        
+    sen_idx = [w for w in words if w not in {word_map['<start>']}]
+    sentence = ' '.join([rev_word_map[sen_idx[k]] for k in range(len(sen_idx))])
+    
+    return sentence
