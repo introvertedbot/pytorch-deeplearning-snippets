65e33e4ebf0940946d1733f5a82e66ea9a5e9b70
introvertedbot <introvertedbot@protonmail.com>
2021-01-13 12:00:58 +0530

    Finished data loading and masking

--- chatbot_transformer.py
+++ chatbot_transformer.py
@@ -107,4 +107,24 @@ class Dataset(Dataset):
     def __len__(self):
         return self.dataset_size
     
-
+train_loader = torch.utils.data.DataLoader(Dataset(),
+                                           batch_size = 100, 
+                                           shuffle=True, 
+                                           pin_memory=True)
+def create_masks(question, reply_input, reply_target):
+    
+    def subsequent_mask(size):
+        mask = torch.triu(torch.ones(size, size)).transpose(0, 1).type(dtype=torch.uint8)
+        return mask.unsqueeze(0)
+    
+    question_mask = question!=0
+    question_mask = question_mask.to(device) 
+    question_mask = question_mask.unsqueeze(1).unsqueeze(1)         # (batch_size, 1, 1, max_words)
+     
+    reply_input_mask = reply_input!=0
+    reply_input_mask = reply_input_mask.unsqueeze(1)  # (batch_size, 1, max_words)
+    reply_input_mask = reply_input_mask & subsequent_mask(reply_input.size(-1)).type_as(reply_input_mask.data) 
+    reply_input_mask = reply_input_mask.unsqueeze(1) # (batch_size, 1, max_words, max_words)
+    reply_target_mask = reply_target!=0              # (batch_size, max_words)
+    
+    return question_mask, reply_input_mask, reply_target_mask
