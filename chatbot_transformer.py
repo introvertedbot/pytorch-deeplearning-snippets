cbe9c72dfc03d136233c6918dff055f920090a10
introvertedbot <introvertedbot@protonmail.com>
2021-01-13 12:09:34 +0530

    Finished decoder layer

--- chatbot_transformer.py
+++ chatbot_transformer.py
@@ -229,4 +229,23 @@ class EncoderLayer(nn.Module):
         return encoded
     
 
+class DecoderLayer(nn.Module):
+    
+    def __init__(self, d_model, heads):
+        super(DecoderLayer, self).__init__()
+        self.layernorm = nn.LayerNorm(d_model)
+        self.self_multihead = MultiHeadAttention(heads, d_model)
+        self.src_multihead = MultiHeadAttention(heads, d_model)
+        self.feed_forward = FeedForward(d_model)
+        self.dropout = nn.Dropout(0.1)
+        
+    def forward(self, embeddings, encoded, src_mask, target_mask):
+        query = self.dropout(self.self_multihead(embeddings, embeddings, embeddings, target_mask))
+        query = self.layernorm(query + embeddings)
+        interacted = self.dropout(self.src_multihead(query, encoded, encoded, src_mask))
+        interacted = self.layernorm(interacted + query)
+        feed_forward_out = self.dropout(self.feed_forward(interacted))
+        decoded = self.layernorm(feed_forward_out + interacted)
+        return decoded
+    
 
