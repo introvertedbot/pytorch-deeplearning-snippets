f8faeca99650e25c083202689e062db46839a18b
introvertedbot <introvertedbot@protonmail.com>
2021-01-13 12:08:36 +0530

    Finished encoder layer

--- chatbot_transformer.py
+++ chatbot_transformer.py
@@ -212,4 +212,21 @@ class FeedForward(nn.Module):
         out = self.fc2(self.dropout(out))
         return out
 
+class EncoderLayer(nn.Module):
+
+    def __init__(self, d_model, heads):
+        super(EncoderLayer, self).__init__()
+        self.layernorm = nn.LayerNorm(d_model)
+        self.self_multihead = MultiHeadAttention(heads, d_model)
+        self.feed_forward = FeedForward(d_model)
+        self.dropout = nn.Dropout(0.1)
+
+    def forward(self, embeddings, mask):
+        interacted = self.dropout(self.self_multihead(embeddings, embeddings, embeddings, mask))
+        interacted = self.layernorm(interacted + embeddings)
+        feed_forward_out = self.dropout(self.feed_forward(interacted))
+        encoded = self.layernorm(feed_forward_out + interacted)
+        return encoded
+    
+
 
