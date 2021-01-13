3782b86c1763e61e7dd5e661bd87792886bd3406
introvertedbot <introvertedbot@protonmail.com>
2021-01-13 12:10:30 +0530

    Finished transformer

--- chatbot_transformer.py
+++ chatbot_transformer.py
@@ -248,4 +248,33 @@ class DecoderLayer(nn.Module):
         decoded = self.layernorm(feed_forward_out + interacted)
         return decoded
     
-
+class Transformer(nn.Module):
+    
+    def __init__(self, d_model, heads, num_layers, word_map):
+        super(Transformer, self).__init__()
+        
+        self.d_model = d_model
+        self.vocab_size = len(word_map)
+        self.embed = Embeddings(self.vocab_size, d_model)
+        self.encoder = nn.ModuleList([EncoderLayer(d_model, heads) for _ in range(num_layers)])
+        self.decoder = nn.ModuleList([DecoderLayer(d_model, heads) for _ in range(num_layers)])
+        self.logit = nn.Linear(d_model, self.vocab_size)
+        
+    def encode(self, src_words, src_mask):
+        src_embeddings = self.embed(src_words)
+        for layer in self.encoder:
+            src_embeddings = layer(src_embeddings, src_mask)
+        return src_embeddings
+    
+    def decode(self, target_words, target_mask, src_embeddings, src_mask):
+        tgt_embeddings = self.embed(target_words)
+        for layer in self.decoder:
+            tgt_embeddings = layer(tgt_embeddings, src_embeddings, src_mask, target_mask)
+        return tgt_embeddings
+        
+    def forward(self, src_words, src_mask, target_words, target_mask):
+        encoded = self.encode(src_words, src_mask)
+        decoded = self.decode(target_words, target_mask, encoded, src_mask)
+        out = F.log_softmax(self.logit(decoded), dim = 2)
+        return out
+    
