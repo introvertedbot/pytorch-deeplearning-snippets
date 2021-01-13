019ae37865cc2d01b6fa9e65e83e472de68c3ac9
introvertedbot <introvertedbot@protonmail.com>
2021-01-13 12:06:47 +0530

    Finished feedforward network

--- chatbot_transformer.py
+++ chatbot_transformer.py
@@ -198,4 +198,18 @@ class MultiHeadAttention(nn.Module):
         interacted = self.concat(context)
         return interacted
     
+class FeedForward(nn.Module):
+
+    def __init__(self, d_model, middle_dim = 2048):
+        super(FeedForward, self).__init__()
+        
+        self.fc1 = nn.Linear(d_model, middle_dim)
+        self.fc2 = nn.Linear(middle_dim, d_model)
+        self.dropout = nn.Dropout(0.1)
+
+    def forward(self, x):
+        out = F.relu(self.fc1(x))
+        out = self.fc2(self.dropout(out))
+        return out
+
 
